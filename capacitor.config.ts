16cf68a6d2aeadb5dcc800b26ea880b49adf237b
Joshua Mantilla <joshua.emmanuel1996@gmail.com>
2023-01-23 21:36:45 +0800

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.random.news.ph',
+  appName: 'news-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
